7d9113f268d49f4dcc3952f8be23198b09bd3d60
tharte823 <147952314+tharte823@users.noreply.github.com>
2023-11-17 18:52:54 -0500

    Update streamlit_app.py
    added icons and 2nd header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.tet('🥑 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
